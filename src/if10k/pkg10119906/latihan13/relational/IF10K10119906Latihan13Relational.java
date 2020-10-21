96b78d15a5b1ac1cc4ea80fa5844ada675ff8b0c
rizqyanandar20 <rizqyananda.rusmana@gmail.com>
2020-10-21 07:04:10 +0700

    first commit

---
+++ src/if10k/pkg10119906/latihan13/relational/IF10K10119906Latihan13Relational.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if10k.pkg10119906.latihan13.relational;
+
+/**
+ *
+ * @author senenngahenen
+ * Nama      : Rizqy Ananda Rusmana
+ * NIM       : 10119906
+ * Kelas     : IF-10K
+ * Deskripsi : Relational.
+ */
+public class IF10K10119906Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        int bill = 10;
+        int bill2 = 20;
+        
+        System.out.println("a == b = " + (bill == bill2));
+        System.out.println("a != b = " + (bill != bill2));
+        System.out.println("a > b = " + (bill > bill2));
+        System.out.println("a < b = " + (bill < bill2));
+        System.out.println("a >= b = " + (bill2 >= bill));
+        System.out.println("a <= b = " + (bill2 <= bill));
+        
+    }
+    
+}
